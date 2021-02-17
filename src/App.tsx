d93ffb7a62dd7fe629d81456bd5bc2eb2325f26c
zpskek <wjdghdwns0@gmail.com>
2021-02-17 14:38:13 +0900

    React Props and Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-
+import React, { Component } from "react";
+import Number from "./Number";
 
 interface IState {
   counter: number;
@@ -7,20 +7,20 @@ interface IState {
 
 class App extends Component<{}, IState> {
   state = {
-    counter: 0
+    counter: 0,
   };
   render() {
     const { counter } = this.state;
     return (
       <div>
-        {counter} <button onClick={this.add}>Add</button>
+        <Number count={counter} /> <button onClick={this.add}>Add</button>
       </div>
     );
   }
   add = () => {
-    this.setState(prev => {
+    this.setState((prev) => {
       return {
-        counter: prev.counter + 1
+        counter: prev.counter + 1,
       };
     });
   };
