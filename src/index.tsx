f5780a6a6afd472440ebdcd27eb5c5f2ae5c8df9
zpskek <wjdghdwns0@gmail.com>
2021-02-17 15:43:13 +0900

    Styled Components and Typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import App from './App';
-
+import ReactDOM from "react-dom";
+import App from "./App";
+import { ThemeProvider } from "styled-components";
+import theme from "./theme";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ThemeProvider theme={theme}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </ThemeProvider>,
+  document.getElementById("root")
 );
